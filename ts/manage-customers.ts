5510a0f6e5edbabf3b7b0530df02ace3db54de85
ChandimaHerath <chandimaherath@hotmail.com>
2021-10-21 16:23:51 +0530

    manage customers added

--- ts/manage-customers.ts
+++ ts/manage-customers.ts
@@ -1,147 +1,182 @@
-import $ from 'jquery';
 import { Customer } from "./dto/customer";
+import $ from 'jquery';
+
+// const BASE_API = 'https://bc677221-4831-4411-b038-9e174414f8ff.mock.pstmn.io';
+const BASE_API = 'http://localhost:8080/pos';
+const CUSTOMERS_SERVICE_API = `${BASE_API}/customers`;
+const PAGE_SIZE = 6;
+const PAGINATION = new Pagination($('.pagination'), PAGE_SIZE, 0, loadAllCustomers);
 
 let customers: Array<Customer> = [];
 let totalCustomers = 0;
-const BASE_API = 'http://localhost:8080/pos';
-const CUSTOMERS_SERVICE_API = `${BASE_API}/customers`;
+/* let selectedPage = 1;
+let pageCount = 1; */
 
 loadAllCustomers();
 
-
-function loadAllCustomers():void{
-    const http = new XMLHttpRequest();
-
-    http.onreadystatechange = () => {
-
-        if (http.readyState === http.DONE) {
-
-            if (http.status !== 200) {
-                alert("Failed to fetch customers, try again...!");
-                return;
-            }
-
-            totalCustomers = +(http.getResponseHeader('X-Total-Count'));
-            customers = JSON.parse(http.responseText);
-
-            $('#tbl-customers tbody tr').remove();
-
-            customers.forEach((c) => {
-                const rowHtml = `<tr>
-                 <td>${c.id}</td>
-                 <td>${c.name}</td>
-                 <td>${c.address}</td>
-                 <td><i class="fas fa-trash"></i></td>
-                 </tr>` ;
-
-
-                $('#tbl-customers tbody').append(rowHtml);
-            });
-        }
-
-    };
-
-    http.open('GET', CUSTOMERS_SERVICE_API , true);
-
-
-    http.send();
-
-};
-
-
-$("#btn-save").on('click', (eventData) => {
-    console.log("save");
+$('#btn-save').on('click', (eventData) => {
     eventData.preventDefault();
-
-    const txtId = $("#txt-id");
-    const txtName = $("#txt-name");
-    const txtAddress = $("#txt-address");
-
+    
+    const txtId = $('#txt-id');
+    const txtName = $('#txt-name');
+    const txtAddress = $('#txt-address');
+    
     let id = (txtId.val() as string).trim();
     let name = (txtName.val() as string).trim();
     let address = (txtAddress.val() as string).trim();
-
+    
     let validated = true;
-
     $('#txt-id, #txt-name, #txt-address').removeClass('is-invalid');
-
+    
     if (address.length < 3) {
         txtAddress.addClass('is-invalid');
         txtAddress.trigger('select');
         validated = false;
     }
-
+    
     if (!/^[A-Za-z ]+$/.test(name)) {
         txtName.addClass('is-invalid');
         txtName.trigger('select');
         validated = false;
     }
-
+    
     if (!/^C\d{3}$/.test(id)) {
         txtId.addClass('is-invalid');
         txtId.trigger('select');
         validated = false;
     }
-
-    if (!validated) {
+    
+    if (!validated) return;
+    
+    if (txtId.attr('disabled')) {
+        
+        const selectedRow = $("#tbl-customers tbody tr.selected");
+        updateCustomer(new Customer(id, name, address));
         return;
     }
-
+    
     saveCustomer(new Customer(id, name, address));
 });
 
-function saveCustomer(customer: Customer): void {
+$('#tbl-customers tbody').on('click', 'tr', function () {
 
-    const http = new XMLHttpRequest();
+    const id = $(this).find("td:first-child").text();
+    const name = $(this).find("td:nth-child(2)").text();
+    const address = $(this).find("td:nth-child(3)").text();
 
-    http.onreadystatechange = () => {
+    $('#txt-id').val(id).attr('disabled', "true");
+    $('#txt-name').val(name);
+    $('#txt-address').val(address);
 
-        if (http.readyState !== http.DONE) return;
+    $("#tbl-customers tbody tr").removeClass('selected');
+    $(this).addClass('selected');
 
-        if (http.status !== 201){
-            alert("Failed to save the customer, retry");
-            return;
-        }
+});
 
-        alert("Customer has been saved successfully");
-        //navigateToPage(pageCount);
-        $("#txt-id, #txt-name, #txt-address").val('');
-        $("#txt-id").trigger('focus');
-
-    };
-
-    http.open('POST', CUSTOMERS_SERVICE_API, true);
-
-    // 4. 
-    http.setRequestHeader('Content-Type', 'application/json');
-
-    http.send(JSON.stringify(customer));
-
-}
-
-function deleteCustomer(id: string): void {
-    const http = new XMLHttpRequest();
-
-    http.onreadystatechange = () => {
-
-        if (http.readyState === http.DONE) {
-
-            if (http.status !== 204) {
-                alert("Failed to delete customer, try again...!");
-                return;
-            }
-
-        
-        }
-
-    };
-
-    http.open('DELETE', CUSTOMERS_SERVICE_API + `?id=${id}`, true);
-
-    http.send();
-}
+$('#tbl-customers tbody').on('click', '.trash', function (eventData) {
+    if (confirm('Are you sure to delete?')) {
+        deleteCustomer(($(eventData.target).parents("tr").find('td:first-child')).text());
+    }
+});
 
 $('#btn-clear').on('click', () => {
     $("#tbl-customers tbody tr.selected").removeClass('selected');
     $("#txt-id").removeAttr('disabled').trigger('focus');
 });
+
+function loadAllCustomers(): void {
+    fetch(CUSTOMERS_SERVICE_API + "?" + new URLSearchParams({page: PAGINATION.selectedPage + "", size: PAGE_SIZE+ ""})).then((resp)=>{
+        if(resp.status !== 200) throw new Error("Failed to load customers, try again");
+
+        totalCustomers = +resp.headers.get('X-Total-Count');
+        return resp.json();
+    }).then((data)=>{
+        customers = data;
+
+        $('#tbl-customers tbody tr').remove();
+
+        customers.forEach((c) => {
+            const rowHtml = `<tr>
+                <td>${c.id}</td>
+                <td>${c.name}</td>
+                <td>${c.address}</td>
+                <td><i class="fas fa-trash trash"></i></td>
+                </tr>` ;
+
+
+            $('#tbl-customers tbody').append(rowHtml);
+        });
+
+        PAGINATION.reInitialize(totalCustomers, PAGINATION.selectedPage);
+    }).catch((err)=>{
+        alert(err.message);
+        console.log(err);
+        
+    });
+}
+
+function updateCustomer(customer: Customer): void {
+
+    fetch(CUSTOMERS_SERVICE_API,{
+        method: 'PUT',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(customer)
+    }).then((resp)=>{
+        if(resp.status !== 204) throw new Error("Failed to update the customer, try again");
+
+        alert("Customer has been updated successfully");
+        $("#tbl-customers tbody tr.selected").find("td:nth-child(2)").text($("#txt-name").val() as string);
+        $("#tbl-customers tbody tr.selected").find("td:nth-child(3)").text($("#txt-address").val() as string);
+        $('#txt-id, #txt-name, #txt-address').val('');
+        $('#txt-id').trigger('focus');
+        $("#tbl-customers tbody tr.selected").removeClass('selected');
+        $('#txt-id').removeAttr('disabled');
+    }).catch((err)=>{
+        alert(err.message);
+        console.error(err);
+    });
+    
+}
+
+function saveCustomer(customer: Customer): void {
+    fetch(CUSTOMERS_SERVICE_API,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(customer)
+    }).then((resp)=>{
+        if(resp.status !== 201) throw new Error("Failed to save customer, try again");
+
+        alert('Customer saved successful');
+        totalCustomers++;
+
+        PAGINATION.pageCount = Math.ceil(totalCustomers / PAGE_SIZE);
+        PAGINATION.navigateToPage(PAGINATION.pageCount);
+        $('#txt-id, #txt-name, #txt-address').val('');
+        $('#txt-id').trigger('focus');
+    }).catch((err)=>{
+        alert(err.message);
+        console.log(err);
+        $('#txt-id').trigger('select');
+    });
+}
+
+function deleteCustomer(id: string): void {
+
+    fetch(CUSTOMERS_SERVICE_API +`?${new URLSearchParams({id: id})}`,{
+        method: 'DELETE'
+    }).then((resp)=>{
+        if(resp.status !== 204) throw new Error("Failed to delete the customer, try again");
+        
+        totalCustomers--;
+      
+
+        $('#btn-clear').trigger('click');
+    }).catch((err)=>{
+        alert(err.message);
+        console.error(err);
+    });
+}
